c4bffc007480be1cfa8a96d759fa84ca2319b209
Ajaxfnc <ajaxfncyt@gmail.com>
2025-05-27 07:14:01 -0500

    example: added an example for using fnapi-js and fnbr.js in one script.

---
+++ src/examples/fnbrjsAndfnapi.js
@@ -0,0 +1,63 @@
+const { Client } = require('fnbr');
+const { ApiClient, Enums, SearchOptions } = require('fnapi-js');
+const { SearchSource } = require('jest');
+
+const client = new Client();
+
+const fnApi = new ApiClient({});
+
+const handleCommand = async (m) => {
+    // console.log(m.content);
+    if (!m.content.startsWith('!')) return;
+    const args = m.content.slice(1).split(' ');
+    const command = args.shift().toLowerCase();
+  
+    if (command === 'outfit' || command === 'skin') {
+      const search = new SearchOptions()
+        .setName(`${args.join(' ')}`)
+        .setMatchMethod(Enums.matchMethod.contains())
+        .setType(Enums.cosmeticType.outfit());
+
+      const skin = await fnApi.cosmetics.search(search, Enums.requestFlags.paths(), false);
+      if (!skin) {
+        console.log(`The skin ${args.join(' ')} wasn't found!`);
+        return;
+      }
+  
+      await m.client.party.me.setOutfit(skin.body().data.id, undefined, undefined);
+      await m.reply(`Set the skin to ${skin.body().data.name}!`);
+
+    } else if (command === 'emote' || command === 'dance') {
+
+      const search = new SearchOptions()
+        .setName(`${args.join(' ')}`)
+        .setMatchMethod(Enums.matchMethod.contains())
+        .setType(Enums.cosmeticType.emote());
+
+      const emote = await fnApi.cosmetics.search(search, Enums.requestFlags.paths(), false);
+      if (!emote) {
+        console.log(`The emote ${args.join(' ')} wasn't found!`);
+        return;
+      }
+  
+      try {
+        await m.client.party.me.setEmote(emote.body().data.id, fnApi.misc.fixPath(emote.body().data.path));
+        await m.reply(`Set the emote to ${emote.body().data.name}!`);
+      } catch (error) {
+        console.log(error);
+        await m.reply(`Failed to set the emote due to: ${error.message}`);
+      }
+    }
+  };  
+
+client.on('friend:message', (message) => {
+  console.log(`Message from ${message.author.displayName}: ${message.content}`);
+  
+  handleCommand(message);
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.displayName}`);
+});
+
+client.login();
